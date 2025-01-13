273bb1ba1f5e7b451fd2e6ef2e8fc41675c45862
NT4105 <tunguyen04105@gmail.com>
2025-01-13 11:25:31 +0700

    feat(Q1: upload singly linked list

--- Q1/src/q1/MyList.java
+++ Q1/src/q1/MyList.java
@@ -140,4 +140,158 @@ public class MyList {
         delete(p);
     }
 
+    // 13. add a node with value x before the node p.
+    public void addBefore(Node p, int x) {
+        Node q = new Node(x);
+        q.next = p;
+        p.next = q;
+    }
+
+    // 14. sort the list in ascending order of info
+    public void sort() {
+        Node p = head;
+        while (p != null) {
+            Node q = p.next;
+            while (q != null) {
+                if (p.info > q.info) {
+                    int temp = p.info;
+                    p.info = q.info;
+                    q.info = temp;
+                }
+                q = q.next;
+            }
+        }
+    }
+
+    // 15. Reverse a singly linked list using only one pass through the list
+    public void reverse() {
+        Node p = head;
+        Node q = null;
+        while (p != null) {
+            Node temp = p.next;
+            p.next = q;
+            q = p;
+            p = temp;
+        }
+        head = q;
+    }
+
+    // 16. create and return array containing info of all nodes in the list
+    public int[] toArray() {
+        int[] arr = new int[size()];
+        Node p = head;
+        for (int i = 0; i < size(); i++) {
+            arr[i] = p.info;
+            p = p.next;
+        }
+        return arr;
+    }
+
+    // 17. Merge two ordered singly linked lists of integers into one ordered list.
+    public void merge(MyList list1, MyList list2) {
+        Node p = list1.head;
+        Node q = list2.head;
+        while (p != null && q != null) {
+            if (p.info < q.info) {
+                addToTail(p.info);
+                p = p.next;
+            } else {
+                addToTail(q.info);
+                q = q.next;
+            }
+        }
+    }
+
+    // 18. Attach a singly linked list to the end of another singly linked list.
+    public void attach(MyList list) {
+        tail.next = list.head;
+        tail = list.tail;
+    }
+
+    // 19. Find and return the maximum value in the list.
+    public int max() {
+        Node p = head;
+        int max = p.info;
+        while (p != null) {
+            if (p.info > max) {
+                max = p.info;
+            }
+            p = p.next;
+        }
+        return max;
+    }
+
+    // 20. Find and return the minimum value in the list
+    public int min() {
+        Node p = head;
+        int min = p.info;
+        while (p != null) {
+            if (p.info < min) {
+                min = p.info;
+            }
+            p = p.next;
+        }
+        return min;
+    }
+
+    // 21. Return the sum of all values in the list.
+    public int sum() {
+        Node p = head;
+        int sum = 0;
+        while (p != null) {
+            sum += p.info;
+            p = p.next;
+        }
+        return sum;
+    }
+
+    // 22. Return the average of all values in the list.
+    public double average() {
+        return sum() / size();
+    }
+
+    // 23. Check and return true if the list is sorted, return false if the list is
+    // not sorted.
+    public boolean isSorted() {
+        Node p = head;
+        while (p != null && p.next != null) {
+            if (p.info > p.next.info) {
+                return false;
+            }
+            p = p.next;
+        }
+        return true;
+    }
+
+    // 24. Insert node with value x into sorted list so that the new list is sorted
+    public void insert(int x) {
+        Node p = head;
+        while (p != null && p.info < x) {
+            p = p.next;
+        }
+        addBefore(p, x);
+    }
+
+    // 25. Check whether two singly linked list have the same contents.
+    public boolean same(MyList list) {
+        Node p = head;
+        Node q = list.head;
+        while (p != null && q != null) {
+            if (p.info != q.info) {
+                return false;
+            }
+            p = p.next;
+            q = q.next;
+        }
+        return true;
+    }
+
+    // Function display the list
+    public void display() {
+        Node p = head;
+        while (p != null) {
+            System.out.println(p.info);
+            p = p.next;
+        }
+    }
 }
--- Q1/src/q1/Q1.java
+++ Q1/src/q1/Q1.java
@@ -3,7 +3,52 @@ package q1;
 public class Q1 {
 
     public static void main(String[] args) {
+        Node p = new Node();
         MyList list = new MyList();
+
+        list.addToTail(6);
+        list.addToHead(4);
+        list.addToTail(5);
+        list.addToHead(7);
+        list.addToTail(3);
+        list.addToHead(1);
+        list.addToTail(3);
+        list.addToHead(5);
+        System.out.print("Created list: ");
+        list.display();
+
+        int count = list.count();
+
+        System.out.println("Numbers of nodes in the list: " + count);
+
+        // Add a node with value x after the node p
+        list.addAfter(p, 10);
+        System.out.print("List after adding a node with value 10 after the node p: ");
+        list.display();
+
+        // Traverse from head to tail and display info of all nodes in the list
+        list.traverse();
+
+        // Count and return number of nodes in the list
+        System.out.println("Numbers of nodes in the list: " + count);
+
+        // Delete the head and return its info
+        int x = list.deleteFromHead();
+        System.out.println("Deleted node with value: " + x);
+        System.out.print("List after deleting the head: ");
+        list.display();
+
+        // Delete the tail and return its info
+        x = list.deleteFromTail();
+        System.out.println("Deleted node with value: " + x);
+        System.out.print("List after deleting the tail: ");
+        list.display();
+
+        // Delete the node after the node p and return its info
+        x = list.deleteAfter(p);
+        System.out.println("Deleted node with value: " + x);
+        System.out.print("List after deleting the node after the node p: ");
+        list.display();
     }
 
 }
