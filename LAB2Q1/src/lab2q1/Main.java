28a963ebdad2149029e83b58aefcfe36f521f0ba
NT4105 <tunguyen04105@gmail.com>
2025-01-17 22:04:08 +0700

    feat(17/1): upload LAB2Q1

--- LAB2Q1/src/lab2q1/Main.java
+++ LAB2Q1/src/lab2q1/Main.java
@@ -1,21 +1,35 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab2q1;
 
-/**
- *
- * @author ACER
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MyStack stack = new MyStack();
+
+        // Test push and traverse
+        System.out.println("Pushing elements: 5, 10, 15, 20");
+        stack.push(5);
+        stack.push(10);
+        stack.push(15);
+        stack.push(20);
+        stack.traverse();
+
+        // Test top
+        System.out.println("Top element: " + stack.top());
+
+        // Test pop
+        System.out.println("Popped element: " + stack.pop());
+        stack.traverse();
+
+        // Test decimal to binary conversion
+        int decimal = 25;
+        System.out.println("\nConverting " + decimal + " to binary:");
+        System.out.println("Binary: " + stack.decimalToBinary(decimal));
+
+        // Test clear and isEmpty
+        System.out.println("\nIs stack empty? " + stack.isEmpty());
+        System.out.println("Clearing stack...");
+        stack.clear();
+        System.out.println("Is stack empty? " + stack.isEmpty());
     }
-    
+
 }
--- LAB2Q1/src/lab2q1/MyStack.java
+++ LAB2Q1/src/lab2q1/MyStack.java
@@ -1,14 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lab2q1;
 
-/**
- *
- * @author ACER
- */
+import java.util.EmptyStackException;
+
 public class MyStack {
-    
+    private Node top;
+
+    public MyStack() {
+        top = null;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void clear() {
+        top = null;
+    }
+
+    public void push(int x) {
+        Node newNode = new Node(x);
+        newNode.setNext(top);
+        top = newNode;
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        int value = top.getData();
+        top = top.getNext();
+        return value;
+    }
+
+    public int top() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return top.getData();
+    }
+
+    public void traverse() {
+        Node current = top;
+        System.out.print("Stack (top to bottom): ");
+        while (current != null) {
+            System.out.print(current.getData() + " ");
+            current = current.getNext();
+        }
+        System.out.println();
+    }
+
+    public String decimalToBinary(int decimal) {
+        if (decimal == 0)
+            return "0";
+
+        MyStack stack = new MyStack();
+        while (decimal > 0) {
+            stack.push(decimal % 2);
+            decimal = decimal / 2;
+        }
+
+        StringBuilder binary = new StringBuilder();
+        while (!stack.isEmpty()) {
+            binary.append(stack.pop());
+        }
+        return binary.toString();
+    }
 }
